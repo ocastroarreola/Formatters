e74b3904e88348f94050bb008d413947ba15341f
omarjair <ocastroarreola@gmail.com>
2015-09-16 12:24:28 -0500

    Initial Commit

---
+++ Formatters/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Formatters
+//
+//  Created by Omar Jair Castro Arreola on 9/16/15.
+//  Copyright (c) 2015 Omar Jair Castro Arreola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Formatters/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Formatters
+//
+//  Created by Omar Jair Castro Arreola on 9/16/15.
+//  Copyright (c) 2015 Omar Jair Castro Arreola. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
